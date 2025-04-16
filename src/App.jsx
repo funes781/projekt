f24a81d128475c4885aaa9a57298dc21c68025ae
funes781 <121256208+funes781@users.noreply.github.com>
2025-04-16 09:19:57 +0200

    Project Navbar initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
 
+import Navbar from './components/Pages/Navbar';
+
 import './App.css'
 
 function App() {
 
   return (
-      <BrowserRouter>
-        <AppContent />
-      </BrowserRouter>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   )
 }
 
-function AppContent(){
+function AppContent() {
   return (
     <div className={`min-h-screen w-full select-none bg-white`}>
-      
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Pages/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import '../../App.css';
+
+function Navbar(){
+    return(
+        <div>
+            <header className="bg-white shadow-md sticky top-0 z-50">
+                <h1>chuj</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Navbar;
